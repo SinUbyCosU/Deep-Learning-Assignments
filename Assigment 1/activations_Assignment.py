b508a7d8b95bf54467de2065f08c1b4490c41455
SinUbyCosU <yadav23@iiserb.ac.in>
2026-02-03 23:16:31 +0530

    parameters

--- Assigment 1/activations_Assignment.py
+++ Assigment 1/activations_Assignment.py
@@ -8,3 +8,11 @@ import time
 
 # Define transformations for the training and test sets
 device=torch.device('cuda' if torch.cuda.is_available() else'cpu')
+batch_size=64
+learning_rate=0.001
+epochs=10
+hidden_state=128
+num_classes=10
+
+transform=transforms.Compose([transforms.ToTensor(),
+                              transforms.Normalize((0.5,), (0.5,))])
